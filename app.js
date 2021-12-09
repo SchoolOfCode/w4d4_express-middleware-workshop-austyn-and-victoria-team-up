bad936348c4483d7abc3da38117ca2af50bbe920
atemp678 <ajt2158@columbia.edu>
2021-12-09 12:31:29 +0000

    updatedstuff

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ app.use(express.json());
 
 // First, structure your middleware so that it uses console.log to output "Request received!" each time your server receives a request.
 
-app.use(function (req, res, next){
-  console.log("got body", req.body); 
-  res.json({message: "Request received!"});
-  next()
+app.use(function (req, res, next) {
+  console.log("got body", req.body);
+  // res.json({message: "Request received!"});
+  if ("POST") {
+    res.json({ timestamp: Date(), ...req.body });
+  }
+  if ("PATCH") {
+    res.json({ timestamp: Date(), ...req.body });
+  }
+  next();
 });
-
 // Run your server and test this to make sure that the console.log appears when a request comes in, and the request is still handled correctly.
 // Once your middleware is working, refactor it so that it also logs the body of the request in the console.
-
-
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
